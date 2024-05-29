da9e11382f5563f9a4ef93146916791e02ac8fa9
Mahmut hüdayi Hançerli <m.hudayihancerli@gmail.com>
2024-05-29 22:13:02 +0300

    Add project files.

---
+++ InvoiceXsltStudio/Program.cs
@@ -0,0 +1,15 @@
+using InvoiceXsltStudio;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Sockets;
+using System.Net;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var app = builder.Build();
+
+await app.RunAsync();
